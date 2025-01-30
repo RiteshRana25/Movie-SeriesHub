3ecca50c4ce7c51790751915a0558adb03ef2180
RiteshRana25 <160099737+RiteshRana25@users.noreply.github.com>
2025-01-31 00:10:29 +0530

    Update Trending.js

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -8,9 +8,11 @@ const Trending = () => {
   const [page, setPage] = useState(1)
   const [content, setContent] = useState([]);
   const [numOfPages, setNumOfPages] = useState()
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
   const fetchtrending = async () => {
     const { data } = await axios.get(
-      `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`
+      `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}&page=${page}`
     );
     setContent(data.results);
     setNumOfPages(data.total_pages);
